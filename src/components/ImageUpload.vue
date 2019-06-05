bb61fff3ca5368e88abe7633c59d66dc7d28a21f
Mick <mick.lynch@gmail.com>
2019-06-05 19:03:45 -0400

    testing test

--- src/components/ImageUpload.vue
+++ src/components/ImageUpload.vue
@@ -1,28 +1,34 @@
 <template>
-    <div>
-        <input type="file" @change="onFileSelected">
-    </div>
+  <div>
+    <input type="file" @change="onFileSelected">
+  </div>
 </template>
 
 <script>
-
-import db from '@/util/db.js'
+import db from "@/util/db.js";
 
 export default {
-    name: 'ImageUpload',
-    data () {
-        return {
-            selectedFile: null,
-        }
-    },
-    methods: {
-        onFileSelected (event) {            
-            this.selectedFile = event.target.files[0]
-            db.collection('image-store').add({
-                name: event.target.value
-            })
-        },
+  name: "ImageUpload",
+  data() {
+    return {
+      selectedFile: null
+    };
+  },
+  methods: {
+    onFileSelected(event) {
+      this.selectedFile = event.target.files[0];
+      // db.collection('image-store').add({
+      //     name: this.selectedFile.name
+      // })
+      db.collection("image-store")
+        .get()
+        .then(querySnapshot => {
+          const documents = querySnapshot.map(doc => doc.data());
+          // do something with documents
+          console.log(documents);
+        });
     }
-}
+  }
+};
 </script>
 
--- src/util/db.js
+++ src/util/db.js
@@ -2,5 +2,7 @@ import firebase from 'firebase'
 
 // Get a Firestore instance
 export default firebase
-  .initializeApp({ projectId: 'image-uploader-242819' })
+  .initializeApp({ 
+      projectId: 'image-uploader-242819'
+    })
   .firestore()
