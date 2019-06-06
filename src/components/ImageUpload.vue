1cdfa7b032cba4326a174a149cbde843b1c64248
Mick <mick.lynch@gmail.com>
2019-06-05 20:05:12 -0400

    working code

--- src/components/ImageUpload.vue
+++ src/components/ImageUpload.vue
@@ -17,17 +17,13 @@ export default {
   methods: {
     onFileSelected(event) {
       this.selectedFile = event.target.files[0];
-      // db.collection('image-store').add({
-      //     name: this.selectedFile.name
-      // })
-      db.collection("image-store")
-        .get()
-        .then(querySnapshot => {
-          const documents = querySnapshot.map(doc => doc.data());
-          // do something with documents
-          console.log(documents);
-        });
+      db.collection('image-store').add({
+          name: this.selectedFile.name
+      })
     }
+  },
+  firestore: {
+
   }
 };
 </script>
--- src/util/db.js
+++ src/util/db.js
@@ -1,8 +1,15 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
 // Get a Firestore instance
 export default firebase
-  .initializeApp({ 
-      projectId: 'image-uploader-242819'
+    .initializeApp({
+        apiKey: 'AIzaSyC4okCUrfZhKfPd0JfGXWdTZLNSBPaJmQM',
+        authDomain: 'image-uploader-242819.firebaseapp.com',
+        databaseURL: 'https://image-uploader-242819.firebaseio.com',
+        projectId: 'image-uploader-242819',
+        storageBucket: 'image-uploader-242819.appspot.com',
+        messagingSenderId: '442782485844',
+        appId: '1:442782485844:web:0d1384ea9fd792ab'
     })
-  .firestore()
+    .firestore()
